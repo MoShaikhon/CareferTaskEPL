81267ffe27e058302b838fe87402cd522c78c8f7
Mohamed Shaikhon <mohamed.shaikhon94@gmail.com>
2023-04-15 21:50:24 +0200

    Initial Commit

---
+++ CareferTaskEPL/CareferTaskEPLApp.swift
@@ -0,0 +1,17 @@
+//
+//  CareferTaskEPLApp.swift
+//  CareferTaskEPL
+//
+//  Created by Mohamad Shaikhon on 15/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct CareferTaskEPLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
